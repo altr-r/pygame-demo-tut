8caa93812eac5e1fa50e54a3e2183f7e85046c27
altr-r <arafatrahman0012@gmail.com>
2023-11-16 01:20:21 +0600

    some erros found, need to fix when I can work on my computer again

--- game.py
+++ game.py
@@ -2,6 +2,9 @@ import sys
 
 import pygame
 
+from scripts.utils import load_image
+from scripts.entities import PhysicsEntity
+
 class Game:
     def __init__(self):
 
@@ -12,33 +15,51 @@ class Game:
 
         self.clock = pygame.time.Clock()
 
-        self.img = pygame.image.load('data/images/clouds/cloud_1.png')
-        self.img.set_colorkey((0, 0, 0))
+        # self.img = pygame.image.load('data/images/clouds/cloud_1.png')
+        # self.img.set_colorkey((0, 0, 0))
 
-        self.img_pos = [160,260]
+        # self.img_pos = [160,260]
         self.movement = [False, False]
+
+        # self.collision_area = pygame.Rect(50, 50 ,300, 50)
+
+        self.assets = {
+            'player' : load_image('entities/player.png')
+        }
+
+        self.player = PhysicsEntity(self, 'player', (50, 50), (8, 15))
 
     def run(self):
         while True: # infinite loop
             self.screen.fill((14, 219, 248))
 
 
-            self.img_pos[1] += (self.movement[1] - self.movement[0]) * 2
-            self.screen.blit(self.img, self.img_pos)
+            # self.img_pos[1] += (self.movement[1] - self.movement[0]) * 2
+            # self.screen.blit(self.img, self.img_pos)
+
+            # img_r = pygame.Rect(self.img_pos[0], self.img_pos[1], self.img.get_width(), self.img.get_height())
+            # if img_r.colliderect(self.collision_area):
+            #     pygame.draw.rect(self.screen, (0, 100, 255), self.collision_area)
+            # else:
+            #     pygame.draw.rect(self.screen, (0, 50, 155), self.collision_area)
+
+
+            self.player.update((self.movement[1] - self.movement[0], 0))
+            self.player.render(self.screen)
 
             for event in pygame.event.get(): #adding the feature of quiting the window
                 if event.type == pygame.QUIT:
                     pygame.quit()
                     sys.exit()
                 if event.type == pygame.KEYDOWN:
-                    if event.key == pygame.K_w:
+                    if event.key == pygame.K_LEFT:
                         self.movement[0] = True
-                    if event.key == pygame.K_s:
+                    if event.key == pygame.K_RIGHT:
                         self.movement[1] = True
                 if event.type == pygame.KEYUP:
-                    if event.key == pygame.K_w:
+                    if event.key == pygame.K_LEFT:
                         self.movement[0] = False
-                    if event.key == pygame.K_s:
+                    if event.key == pygame.K_LEFT:
                         self.movement[1] = False
 
             pygame.display.update() # updating the window
